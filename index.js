fe274ca7ca1ec3ef22fa0a3bed45212c82496611
developerdevsoni <dev.microlentsystem@gmail.com>
2025-01-08 11:37:46 +0530

    check

--- index.js
+++ index.js
@@ -6,6 +6,7 @@ import {AppRegistry} from 'react-native';
 import App from './App';
 import {name as appName} from './app.json';
 import notifee from '@notifee/react-native';
+//dev
 
 notifee.onBackgroundEvent(async ({ type, detail }) => {
     const { notification, pressAction } = detail;
